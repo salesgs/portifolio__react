fc0822c9f0bc6b4d2b61c3cedd6024d067f05a95
Gabriel Sales <gabrielhub16@gmail.com>
2023-08-25 19:44:51 -0300

    cabeçaho 8

--- src/Componentes/Header/index.js
+++ src/Componentes/Header/index.js
@@ -2,10 +2,15 @@ import './Header.css';
 import './Responsive.css';
 const Header = () => {
   return(
-    <header>
+    <header> 
+      
       <div className='top'>
             <div className='top__navigation'>
-              <h2><img src='./images/group.png'/> Gabriel</h2>    
+              <h2>
+                <img src='./images/group.png'/> Gabriel
+                  <div className='photo'></div>
+                </h2>
+            
               <nav className='top__nav'>
                   <ul className='top__list'>
                       <li className='iten'>Sobre mim</li>
@@ -13,13 +18,16 @@ const Header = () => {
                       <li className='iten'>Hobbies</li>
                       <li className='iten'>Formação</li>
                       <li className='iten'>Projetos</li>
-                      <li>Whatsapp</li>
+                      <li>Whatsapp</li>   
                    </ul>
+            
               </nav>
             </div>
       </div>
+              
       <div className='sobre'>
         <div className='sobre__box'>
+        <div className='photo'></div>
           <div className='sobre__text'>
             <h1>Olá meu nome é Gabriel Sales, sou Desenvolvedor Front-end Jr.</h1>
             <p>Sou estudante de Desenvolvimento de sistemas do Senai(BA), formado no programa de formação full-stack na Recode-Pro,e atualmente aluno da Oracle Next Education. Bom estou embusca da minha primeira oportunidade no mundo da tecnologia, espero e irie conseguir logo em breve.
