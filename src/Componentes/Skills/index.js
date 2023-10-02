7f79be27767b88998933ff7c7af679e1fb54bbc3
Gabriel Sales <gabrielhub16@gmail.com>
2023-10-01 22:59:28 -0300

    skills-1

--- src/Componentes/Skills/index.js
+++ src/Componentes/Skills/index.js
@@ -2,44 +2,35 @@ import "./Skills.css"
 import "./Responsive.css"
 const Skills = () =>{
   return(
-     ''
-//     <div className='Skills' id='Skills'>
-//       <div className='Skills__title'>
-//           <h1>Skills</h1>
-//       </div>
-//       <div className='Skills__images'>
-//         <div className='skills__html'>
-//              <img src='./images/html.png' id='html'/> 
-//         </div>
-//         <div className='skills__css'>
-//              <img src='./images/css.png' id='css'/> 
-//         </div>
-        
-//         <div className='skills__js'>
-//              <img src='./images/js.png' id='js'/> 
-//         </div>
-        
-//         <div className='skills__react'>
-//              <img src='./images/react.png' id='react'/> 
-//         </div>
-        
-//         <div className='skills__node'>
-//              <img src='./images/node.png' id='node'/> 
-//         </div>
-        
-//         <div className='skills__git'>
-//              <img src='./images/git.png' id='git'/> 
-//         </div>
-        
-//         <div className='skills__java'>
-//              <img src='./images/java.png' id='java'/> 
-//         </div>
-        
-//         <div className='skills__mysql'>
-//              <img src='./images/mysql.png' id='mysql'/> 
-//         </div>
-//       </div>
-//     </div>
+    <div className='container'>
+        <section className='title'>
+            <h2>Skills</h2>
+        </section>
+        <div className='stacks'>
+            <div className='stack__html'>
+         
+            </div>
+            <div className='stack__css'>
+                
+            </div>
+            
+            <div className='stack__js'>
+                
+            </div>
+            <div className='stack__node'>
+                
+            </div>
+            <div className='stack__react'>
+                
+            </div>
+            
+            <div className='stack__git'>
+            </div>
+            
+            <div className='stack__mysql'>
+            </div>
+        </div>
+    </div>
   )
 }
 export default Skills;
